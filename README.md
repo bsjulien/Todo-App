7e1c900861a5f26cd391685bca452504a1f0f581
Julien Barezi <j.barezi@irembo.com>
2023-05-22 09:07:24 +0200

    updating ...

--- README.md
+++
@@ -1,3 +0,0 @@
-# Todo Web App (Backend)
-
-This project is developed with Java and Spring boot 
--- src/main/java/com/julien/todo/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.julien.todo.utils;
-
-public class Constants {
-
-    public static final String APP_ROOT = "/api";
-}
